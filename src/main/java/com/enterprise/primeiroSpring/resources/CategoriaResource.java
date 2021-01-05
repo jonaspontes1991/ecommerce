fe428e26bda0dff02b0793f0b40828948e60f69a
jonas pontes <pontesjonas060@gmail.com>
2021-01-05 20:27:38 -0300

    Rest testado

---
+++ src/main/java/com/enterprise/primeiroSpring/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.enterprise.primeiroSpring.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value= "/categorias")
+public class CategoriaResource {
+	@RequestMapping(method = RequestMethod.GET)
+	public String metodo() {
+		return "rest funcionando";
+	}
+
+}
